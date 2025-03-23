c5594b227e5f0ddb341a0453cf8a6ab804365f1f
SamiMadane <samimadane23@gmail.com>
2025-03-23 11:23:47 +0200

    - Fake push to development branch

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,8 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:leuko_care/leuko_care.dart';
 import 'core/routes/app_router.dart';
 
-void main() async{
+void main() async{ 
+  
   WidgetsFlutterBinding.ensureInitialized();
   await ScreenUtil.ensureScreenSize();  
   runApp(LeukoCare(
