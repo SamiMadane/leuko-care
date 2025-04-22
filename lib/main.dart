37b9db38b3b8a221cbbfa9c2228cab0b2d4bd729
SamiMadane <samimadane23@gmail.com>
2025-04-22 12:53:48 +0300

    - save stuck app proplem comment and un comment code appcheck

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:leuko_care/core/di/dependency_injection.dart';
+import 'package:leuko_care/firebase_options.dart' show DefaultFirebaseOptions;
 import 'package:leuko_care/leuko_care.dart';
 import 'core/routes/app_router.dart';
 
 void main() async{ 
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
     // ✅ إعداد Debug Provider فقط لبيئة التطوير
   // await FirebaseAppCheck.instance.activate(
   //   androidProvider: AndroidProvider.debug,
