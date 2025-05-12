de6e547ad2dc46342562f1537818fe9d83fc9bea
SamiMadane <samimadane23@gmail.com>
2025-05-12 12:13:41 +0300

    - Move add_update_doctor_screen.dart to shared folder between admin and doctor user

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -11,7 +11,7 @@ import 'package:leuko_care/feature/chats/logic/cubit/chat_cubit.dart';
 import 'package:leuko_care/feature/chats/ui/views/image_preview_screen.dart';
 import 'package:leuko_care/feature/doctors/data/models/doctor_model.dart';
 import 'package:leuko_care/feature/doctors/logic/cubit/doctor_cubit.dart';
-import 'package:leuko_care/feature/doctors/ui/views/admin_user/add_update_doctor_screen.dart';
+import 'package:leuko_care/feature/doctors/ui/views/shared/add_update_doctor_screen.dart';
 import 'package:leuko_care/feature/doctors/ui/views/admin_user/all_doctors_screen.dart';
 import 'package:leuko_care/feature/doctors/ui/views/admin_user/doctor_details_screen.dart';
 import 'package:leuko_care/feature/auth/logic/cubit/auth_cubit.dart';
